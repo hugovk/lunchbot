d97957013def4a6e4eefce5a8b0d613179cd144c
Hugo <hugovk@users.noreply.github.com>
2019-01-11 19:04:29 +0200

    Replace spaces with underscores in emoji

--- lunchbot.py
+++ lunchbot.py
@@ -310,7 +310,8 @@ def lunch_lounaat(restaurant):
     path = element.find("h3 a")[0].attrs["href"]  # eg. '/lounas/presto/helsinki'
     url = f"https://www.lounaat.info{path}"
 
-    todays_menu = ["", f":{restaurant}: {restaurant} {url}", ""]
+    emoji = restaurant.replace(" ", "_")
+    todays_menu = ["", f":{emoji}: {restaurant} {url}", ""]
     opening_hours = element.find("p.lunch", first=True).text
     item_body = element.find("div.item-body", first=True).text
     item_footer = element.find("div.item-footer", first=True).text
